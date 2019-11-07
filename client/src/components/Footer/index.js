bb6c537cfb1f93730e0a60941503382f30d3ba1d
Tahsin Nazmee <tnazmee@yahoo.com>
2019-11-06 23:55:31 -0600

    got app to work

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => (
+	<footer className = "page-footer">
+		<div className = "container-fluid text-center">
+		</div>	
+	</footer>
+);
+
+export default Footer;
---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./style.css";
+
+function Nav() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-primary">
+            <a className="navbar-brand" href="/">
+                <h1 className="text-white">Google Book Search</h1>
+            </a>
+            <div id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item" id="home">
+                        <a className="nav-link" href="/"><button type="button" className="btn btn-info text-white">Search Books</button></a>
+                    </li>
+                    <li className="nav-item" id="report">
+                        <a className="nav-link" href="/saved"><button type="button" className="btn btn-warning text-white">Saved Books</button></a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Nav;
